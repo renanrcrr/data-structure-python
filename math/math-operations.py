20973917eddbe062de37702a3f8fbacaeef10dd2
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-20 21:20:15 +0930

    Adding math operations.

---
+++ math/math-operations.py
@@ -0,0 +1,35 @@
+import math
+
+print(math.ceil(3.14)) # Greatest integer more than 3.14 => 4
+print(math.floor(3.14)) # Greatest integer less than 3.14 => 3
+
+print(math.fabs(-4)) # Absolute value => 4.0
+print(math.fabs(4)) # Absolute value => 4.0
+
+print(math.exp(4)) # Exponent => 4.0
+
+print(math.log10(10)) # log => 1.0
+print(math.log10(100)) # log => 2.0
+print(math.log2(100)) # log => 6.64
+
+print(math.pow(2, 10)) # 1024.0 = 2 ** 10
+print(math.pow(3, 4)) # 3 ** 4
+
+print(math.cos(0)) # 1.0
+print(math.sin(0)) # 0.0
+
+print(math.pi) # 3.14...
+print(math.e) # 7.7182....
+
+
+
+
+
+
+
+
+
+
+
+
+
