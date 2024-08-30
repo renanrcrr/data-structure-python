1f7c9833a7f9b96aac549f015031625f20a768ab
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-30 22:57:55 +0930

    Updating the struture data stack.

--- stack/stack.py
+++ stack/stack.py
@@ -21,14 +21,22 @@ class Stack:
 		if self.len_stack == 0:
 			return True
 		return False
+	
+	def __str__(self):
+		return f'Current stack => {self.stack}'
 
 	def length(self):
 		return self.len_stack
 
 s = Stack()
-
+print('Stack:' , s)
 s.push(11)
+print('New element added =>' , s.top())
 s.push(21)
+print('New element added =>' , s.top())
 s.push(31)
-
-print(s.top())
+print('New element added =>' , s.top())
+print('Stack:' , s)
+s.pop()
+print(s.empty())
+print('Stack:' , s)
