2b356a8a7b2a37f13d3f00f22f362dac9b3c7dde
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-30 23:08:45 +0930

    Updating the structure data queue.

--- queue/queue.py
+++ queue/queue.py
@@ -1,19 +1,15 @@
 class Queue:
-
 	def __init__(self):
 		self.queue = []
-		
 		self.len_queue = 0
 
 	def push(self, e):
 		self.queue.append(e)
-		
 		self.len_queue += 1
 
 	def pop(self):
 		if not self.empty():
 			self.queue.pop(0)
-			
 			self.len_queue -= 1
 
 	def empty(self):
@@ -29,11 +25,16 @@ class Queue:
 			return self.queue[0]
 		return None
 
+	def __str__(self):
+		return f'Current queue => {self.queue}'
 	
 q = Queue()
-
+print(q)
 q.push(1)
 q.push(2)
 q.push(3)
-
+q.push(4)
+print(q)
 print(q.front())
+q.pop()
+print(q)
