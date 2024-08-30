f133d1ab7b5689c6453227b2cf6c86f28e90ef2c
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-30 23:25:45 +0930

    Updating data structure queue.

--- deque/deque.py
+++ deque/deque.py
@@ -34,27 +34,44 @@ class Deque:
 		if not self.empty():
 			return self.deque[-1]
 
-	def show(self):
-		for i in self.deque:
-			print(i, end=' ')
+	# def show(self):
+	# 	for i in self.deque:
+	# 		print(i, end=' ')
+	
+	def __str__(self):
+		return f'Current deque => {self.deque}'
 
 
 d = Deque()
-
-d.push_front(10) 
-d.push_front(5) 
+print(d)
+d.push_front(10)
+print("New element added!")
+print('Front:',d.front())
+print('Back:',d.back()) 
+d.push_front(5)
+print("New element added!") 
+print('Front:',d.front())
+print('Back:',d.back())
 d.push_back(20) 
+print("New element added!")
+print('Front:',d.front())
+print('Back:',d.back())
 d.push_front(7) 
+print("New element added!")
+print('Front:',d.front())
+print('Back:',d.back())
 d.push_back(40) 
-d.show()
-
-print('\nfront: %d' % d.front()) 
-print('back: %d\n' % d.back()) 
+print("New element added!")
+print('Front:',d.front())
+print('Back:',d.back())
+print(d)
 
 d.pop_back() 
 d.pop_back() 
 d.pop_front()
-d.show()
+print("Deleting elements!")
 
 print('\nfront: %d' % d.front()) 
 print('back: %d' % d.back())
+
+print(d)
